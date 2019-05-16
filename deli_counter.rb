2b88ade9f9a2ff07e2b14b2b12f4ea086c4395a3
Tom White <eugene.white@gmail.com>
2019-05-16 19:16:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(deli_line)
   else
     line = []
     deli_line.each_with_index do |name, index|
-      line.push ("#{index + 1}. #{name}")
+      line.push ("#{index + 1}. #{name} ")
+      
     
   end
 end
